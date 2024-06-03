375d88c57d1642ea5d6b234420c6d044f0e645a2
ajayputtamurthy <ajay.puttamurthy@gmail.com>
2024-06-03 20:07:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+import PostsList from "./components/PostsList";
+import MainHeader from "./components/MainHeader";
+
+function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  return (
+    <>
+      <MainHeader onCreatePost={showModalHandler}></MainHeader>
+      <main>
+        <PostsList
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        ></PostsList>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/prop-types */
+
+import Post from "./Post";
+import Modal from "./Modal";
+import NewPost from "./NewPost";
+import classes from "./PostsList.module.css";
+import { useState } from "react";
+
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          <>
+            {posts.map((post) => (
+              // eslint-disable-next-line react/jsx-key
+              <Post author={post.author} body={post.body}></Post>
+            ))}
+          </>
+        </ul>
+      )}
+
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostsList;
